7a209ea51bf5701c0ff709cfd2a2cbfd32012469
Hüseyin Çayırlı <hcayirli97@gmail.com>
2023-02-12 15:54:52 +0300

    v4
    main.py was revised. Morphology operations were changed. Outputs are more clear than before now.

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-import time
+import sys
+
 import torch
-from torch.backends import cudnn
-import sys 
+
 sys.path.append("HybridNets/")
 
-from backbone import HybridNetsBackbone
+import argparse
+import os
+
 import cv2
 import numpy as np
-from glob import glob
-from utils.utils import letterbox, scale_coords, postprocess, BBoxTransform, ClipBoxes, restricted_float, \
-    boolean_string, Params
-from utils.plot import STANDARD_COLORS, standard_to_bgr, get_index_label, plot_one_box
-import os
+from backbone import HybridNetsBackbone
 from torchvision import transforms
-import argparse
 from utils.constants import *
-from collections import OrderedDict
-from torch.nn import functional as F
-
+from utils.utils import (BBoxTransform, ClipBoxes, Params, boolean_string,
+                         letterbox, postprocess, restricted_float,
+                         scale_coords)
+import warnings
+warnings.simplefilter('ignore', np.RankWarning)
 
 def main(args):
     params = Params(f'HybridNets/projects/{args.project}.yml')
@@ -30,14 +29,7 @@ def main(args):
 
     anchors_ratios = params.anchors_ratios
     anchors_scales = params.anchors_scales
-    threshold = args.conf_thresh
-    iou_threshold = args.iou_thresh
 
-
-    color_list_seg = {}
-    for seg_class in params.seg_list:
-        # edit your color here if you wanna fix to your liking
-        color_list_seg[seg_class] = list(np.random.choice(range(256), size=3))
 
     obj_list = params.obj_list
     seg_list = params.seg_list
@@ -75,12 +67,14 @@ def main(args):
     if use_float16:
         model = model.half()
     video_capture = cv2.VideoCapture(source)
+    frame_Counter = 0
     while True:
         ret, image = video_capture.read()
+        frame_Counter += 1
         if ret:
-            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            h0, w0 = image.shape[:2]  # orig hw
-            r = resized_shape / max(h0, w0)  # resize image to img_size
+
+            h0, w0 = image.shape[:2]  
+            r = resized_shape / max(h0, w0)  
             input_img = cv2.resize(image, (int(w0 * r), int(h0 * r)), interpolation=cv2.INTER_AREA)
             h, w = input_img.shape[:2]
 
@@ -90,17 +84,15 @@ def main(args):
             else:
                 x = torch.stack([transform(input_img)], 0)
             x = x.to(torch.float16 if use_cuda and use_float16 else torch.float32)
-            # x.unsqueeze_(0)
             shapes = []
             shapes.append(((h0, w0), ((h / h0, w / w0), pad)))
             with torch.no_grad():
-                features, regression, classification, anchors, seg = model(x)
-                # in case of MULTILABEL_MODE, each segmentation class gets their own inference image
+                _, _, _, _, seg = model(x)
                 seg_mask_list = []
-                # (B, C, W, H) -> (B, W, H)
+
                 if seg_mode == BINARY_MODE:
                     seg_mask = torch.where(seg >= 0, 1, 0)
-                    # print(torch.count_nonzero(seg_mask))
+
                     seg_mask.squeeze_(1)
                     seg_mask_list.append(seg_mask)
                 elif seg_mode == MULTICLASS_MODE:
@@ -108,11 +100,10 @@ def main(args):
                     seg_mask_list.append(seg_mask)
                 else:
                     seg_mask_list = [torch.where(torch.sigmoid(seg)[:, i, ...] >= 0.5, 1, 0) for i in range(seg.size(1))]
-                    # but remove background class from the list
                     seg_mask_list.pop(0)
-                # (B, W, H) -> (W, H)
+
                 for i in range(seg.size(0)):
-                    for seg_class_index, seg_mask in enumerate(seg_mask_list):
+                    for seg_mask in seg_mask_list:
                             seg_mask_ = seg_mask[i].squeeze().cpu().numpy()
                             pad_h = int(shapes[i][1][1][1])
                             pad_w = int(shapes[i][1][1][0])
@@ -123,21 +114,27 @@ def main(args):
                                 if index == 1:
                                     mask = np.where(seg_mask_ == index+1 , 255, 0)
                                     mask = mask.astype(np.uint8)
-                for i in range(3):
-                    outmask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-                contours, hierarchy = cv2.findContours(outmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                outmask = mask.copy()
+                for i in range(2):
+                    outmask = cv2.morphologyEx(outmask, cv2.MORPH_OPEN, kernel)
+                    outmask = cv2.erode(outmask, kernel)
+
+                all_contours, _ = cv2.findContours(outmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                min_area = 2000
+                contours = [cnt for cnt in all_contours if cv2.contourArea(cnt) > min_area]
                 parameters = []
                 for contour in contours:
                     [vx, vy, cx, cy]= cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
-                    cv2.line(image, (int(cx-vx*w), int(cy-vy*w)), (int(cx+vx*w), int(cy+vy*w)), (0, 255, 0), 5)
-                    coefficients = np.polyfit(np.array([int(cx-vx*w),int(cx+vx*w)]),np.array([int(cy-vy*w),int(cy+vy*w)]), 2)
+                    cv2.line(image, (int(cx-vx*w0), int(cy-vy*w0)), (int(cx+vx*w0), int(cy+vy*w0)), (0, 255, 0), 5)
+                    coefficients = np.polyfit(np.array([int(cx-vx*w0),int(cx+vx*w0)]),np.array([int(cy-vy*w0),int(cy+vy*w0)]), 2)
                     parameters.append(coefficients)
-                
-                print(parameters)
+                print("Frame {} ".format(frame_Counter))
+                print("-"*20)
+                for l, par in enumerate(parameters):
+                    print("line {} - Coefficients a:{:.4f} b:{:.4f} c:{:.4f}".format(l, par[0], par[1], par[2]))
         else:
                 break
-
-
 
 
 
@@ -151,12 +148,6 @@ if __name__ == '__main__':
     parser.add_argument('--source', type=str, default='videos/NO20221023-112820-000013F.MP4', help='The demo image folder')
     parser.add_argument('--output', type=str, default='result', help='Output folder')
     parser.add_argument('-w', '--load_weights', type=str, default='weights/hybridnets.pth')
-    parser.add_argument('--conf_thresh', type=restricted_float, default='0.75')
-    parser.add_argument('--iou_thresh', type=restricted_float, default='0.5')
-    parser.add_argument('--imshow', type=boolean_string, default=False, help="Show result onscreen (unusable on colab, jupyter...)")
-    parser.add_argument('--imwrite', type=boolean_string, default=True, help="Write result to output folder")
-    parser.add_argument('--show_det', type=boolean_string, default=False, help="Output detection result exclusively")
-    parser.add_argument('--show_seg', type=boolean_string, default=False, help="Output segmentation result exclusively")
     parser.add_argument('--cuda', type=boolean_string, default=True)
     parser.add_argument('--float16', type=boolean_string, default=True, help="Use float16 for faster inference")
     parser.add_argument('--speed_test', type=boolean_string, default=False,
